21846659d918270375ac399cfdc17c501dfdb4b2
XIN <vialin1128@gmail.com>
2022-09-26 01:55:16 +0800

    tweak configs

--- src/renderer/components/Loader.jsx
+++ src/renderer/components/Loader.jsx
@@ -1,10 +1,10 @@
-import { loader } from "../assets";
+import loader from '../assets/loader.svg';
 
 const Loader = ({ title }) => (
   <div className="w-full flex justify-center items-center flex-col">
     <img src={loader} alt="loader" className="w-32 h-32 object-contain" />
     <h1 className="font-bold text-2xl text-white mt-2">
-      {title || "获取中..."}
+      {title || '获取中...'}
     </h1>
   </div>
 );
